420669d7fee1e5987c1eac2ab9f7ea4a4ed0466c
karanaushikar <karanaushikar03@gmail.com>
2025-10-18 19:32:55 +0530

    minor changes

--- src/pages/AartiGalleryPage.js
+++ src/pages/AartiGalleryPage.js
@@ -5,9 +5,9 @@ import AartiCard from '../components/AartiCard';
 
 // Import all Aarti images
 import GaneshAartiImage from '../assets/images/ganesh-aarti.jpg';
-import ShivAartiImage from '../assets/images/shiv-aarti.jpg';
+// import ShivAartiImage from '../assets/images/shiv-aarti.jpg';
 import DeviAartiImage from '../assets/images/devi-aarti.jpg';
-// import HanumanAartiImage from '../assets/images/hanuman-aarti.jpg'; // Using the placeholder as requested
+import HanumanAartiImage from '../assets/images/hanuman-aarti.jpg'; // Using the placeholder as requested
 // import '../assets/styles/AartiGalleryPage.css'; // Uncomment and create this file for styling
 
 
@@ -18,24 +18,24 @@ const aartisData = [
     description: 'Performed to invoke blessings, Lord Ganesh, the remover of obstacles, before any auspicious beginning.',
     image: GaneshAartiImage,
   },
+  // {
+  //   id: 2,
+  //   title: 'Shiv Aarti',
+  //   description: 'A devotional prayer dedicated to Lord Shiva, the destroyer of evil and transformer, integral to our daily worship and festivals.',
+  //   image: ShivAartiImage,
+  // },
   {
     id: 2,
-    title: 'Shiv Aarti',
-    description: 'A devotional prayer dedicated to Lord Shiva, the destroyer of evil and transformer, integral to our daily worship and festivals.',
-    image: ShivAartiImage,
-  },
-  {
-    id: 3,
     title: 'Devi Aarti',
     description: 'A powerful prayer celebrating the the divine feminine, Devi\'s strength, and protection, especially during Navaratri.',
     image: DeviAartiImage,
   },
-//   {
-//     id: 4,
-//     title: 'Hanuman Aarti',
-//     description: 'A powerful prayer for strength, courage, and devotion, essential for well-being and overcoming challenges.',
-//     image: HanumanAartiImage,
-//   },
+  {
+    id: 3,
+    title: 'Hanuman Aarti',
+    description: 'A powerful prayer for strength, courage, and devotion, essential for well-being and overcoming challenges.',
+    image: HanumanAartiImage,
+  },
 ];
 
 const AartiGalleryPage = () => {
